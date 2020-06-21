bf0e8ae36ca97dc29f20c5b127c6f7b54167c68d
mahim1997 <1505022.mm@ugrad.cse.buet.ac.bd>
2020-06-21 06:02:59 +0600

    Populating initial tables code probably done need to test and check

--- WQFM/src/wqfm/Runner.java
+++ WQFM/src/wqfm/Runner.java
@@ -24,14 +24,14 @@ public class Runner {
 //        testCustomDS();
 
     }
-    static void testCustomDS()
-    {
+
+    static void testCustomDS() {
         CustomDS customDS = new CustomDS();
         customDS.table1_quartets_double_list.add(new ArrayList<>());
         List<Quartet> get = customDS.table1_quartets_double_list.get(0);
         get.add(new Quartet("A", "B", "C", "D", 10));
         customDS.printCustomDS();
-        
+
     }
 
     private static void mainMethod() {
@@ -46,31 +46,47 @@ public class Runner {
     private CustomDS populateCustomTables(List<String> lines) {
         CustomDS customDS = new CustomDS();
         String line;
-        for (int i = 0; i < lines.size(); i++) {
-            line = lines.get(i);
+        for (int iter_lines = 0; iter_lines < lines.size(); iter_lines++) {
+            line = lines.get(iter_lines);
             Quartet quartet = new Quartet(line);
 //            System.out.print(i + ":" + line + "  ");
 //            quartet.printQuartet();
 
+            int row_idx_table_1, col_idx_table_1; //Use this as marker to represent current quartet ... put in map for <taxa, list(r,c)> of relevant_quartets_per_taxa_table3
             //Populate table 2 <USE MAP> [ weight,index_of_table_1_weight ]
-
-            int current_row_idx_of_table_1 = customDS.table1_quartets_double_list.size();
-            
-            int row_idx_table_1, col_idx_table_1;
-            
-            if(customDS.table2_map_weight_indexQuartet.containsKey(quartet.weight) == false){ //First time, THIS weight value is found ...
-                customDS.table2_map_weight_indexQuartet.put(quartet.weight, current_row_idx_of_table_1); //Key doesn't exist, so, THIS ROW (i.e. weight) in table 1 DOES NOT exist, insert in map
+            if (customDS.table2_map_weight_indexQuartet.containsKey(quartet.weight) == false) { //First time, THIS weight value is found ...
+                row_idx_table_1 = customDS.table1_quartets_double_list.size(); // AT THIS moment, no need to subtract 1, since this is BEFORE putting new array_list for columns
+                customDS.table2_map_weight_indexQuartet.put(quartet.weight, row_idx_table_1); //Key doesn't exist, so, THIS ROW (i.e. weight) in table 1 DOES NOT exist, insert in map
                 //Also, initialize table 1's columns' list for THIS weight value (this row)
                 customDS.table1_quartets_double_list.add(new ArrayList<>()); // initialize this [since this is the first time THIS weight is found]
-                List<Quartet> col_list_for_table1_this_weight = customDS.table1_quartets_double_list.get(current_row_idx_of_table_1); // obtain the list.
-                col_list_for_table1_this_weight.add(quartet);
-                
-            }else{ //NOT the first time for THIS weight, THIS weight EXISTS ...
-                
-            }
-            
 
-            
+            } else { //NOT the first time for THIS weight, THIS weight EXISTS ...  // No need to initalize column's list of table 1.
+                row_idx_table_1 = customDS.table2_map_weight_indexQuartet.get(quartet.weight);
+            }
+            List<Quartet> col_list_for_table1_this_weight = customDS.table1_quartets_double_list.get(row_idx_table_1); // obtain the list.
+            col_list_for_table1_this_weight.add(quartet);
+            col_idx_table_1 = customDS.table1_quartets_double_list.get(row_idx_table_1).size() - 1; // Current row's --> columns_list.size - 1 
+
+            //Put in hashMap(relevant_quartetes_per_taxa) for each taxa in the quartet.
+            //For left sisters
+            for (int i = 0; i < Quartet.NUM_TAXA_PER_PARTITION; i++) {
+                String taxa = quartet.taxa_sisters_left[i];
+                if (customDS.map_taxa_relevant_quartet_indices.containsKey(taxa) == false) { // doesn't contain THIS taxa in map, so initialize list and put this taxa as key to map.
+                    customDS.map_taxa_relevant_quartet_indices.put(taxa, new ArrayList<>());
+                }
+                List<Pair<Integer, Integer>> list_indices_of_quartets = customDS.map_taxa_relevant_quartet_indices.get(taxa); // now will exist [since we have initialized previously]
+                list_indices_of_quartets.add(new Pair(row_idx_table_1, col_idx_table_1));
+            }
+
+            //For right sisters
+            for (int i = 0; i < Quartet.NUM_TAXA_PER_PARTITION; i++) {
+                String taxa = quartet.taxa_sisters_right[i];
+                if (customDS.map_taxa_relevant_quartet_indices.containsKey(taxa) == false) { // doesn't contain THIS taxa in map, so initialize list and put this taxa as key to map.
+                    customDS.map_taxa_relevant_quartet_indices.put(taxa, new ArrayList<>());
+                }
+                List<Pair<Integer, Integer>> list_indices_of_quartets = customDS.map_taxa_relevant_quartet_indices.get(taxa); // now will exist [since we have initialized previously]
+                list_indices_of_quartets.add(new Pair(row_idx_table_1, col_idx_table_1));
+            }
         }
 
         return customDS;
