f0cb50aca14673be14a3da142a9c875c65102296
mahim1997 <1505022.mm@ugrad.cse.buet.ac.bd>
2020-06-24 19:53:31 +0600

    working for fm-whole using only level-0 for quartets-map

--- WQFM/src/wqfm/main/Main.java
+++ WQFM/src/wqfm/main/Main.java
@@ -13,9 +13,9 @@ public class Main {
     public static int REROOT_MODE = Status.REROOT_USING_JAR;
     public static int PARTITION_SCORE_MODE = Status.PARTITION_SCORE_MODE_1; //1->[s]-[v], 2->[s]-0.5[v], 3->[s]-[v]-[d]
    
-    public static String INPUT_FILE_NAME = "wqrts_avian_2X_R1";
+//    public static String INPUT_FILE_NAME = "wqrts_avian_2X_R1";
 //    public static String INPUT_FILE_NAME = "wqrts_15G_100g100b_R1";
-//    public static String INPUT_FILE_NAME = "wqrts_11Tax_est_5G_R1";
+    public static String INPUT_FILE_NAME = "wqrts_11Tax_est_5G_R1";
 //    public static String INPUT_FILE_NAME = "wqrts_toy_dataset_QFM_paper";
 
     public static double SMALLEPSILON = 0; //very small number to take the place of 0
