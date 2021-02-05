37d1e82ace7911f41e0757fd9b7f3b06c9ebe3e7
mahim1997 <1505022.mm@ugrad.cse.buet.ac.bd>
2021-02-06 04:23:48 +0600

    Returns map of quartets vs taxa and list of quartets

--- WQFM/scripts-quartet-support/get_branch_qrt_support.py
+++ WQFM/scripts-quartet-support/get_branch_qrt_support.py
@@ -1,36 +1,98 @@
 import dendropy
-
-## https://dendropy.org/primer/bipartitions.html
-
-## ((10,((6,5),(9,(7,8)))),(11,(4,(2,(3,1)))));
-
-## ((5,6),((1,2),(3,4)));
-## (5,(6,((1,2),(3,4))));
-
-taxa = dendropy.TaxonNamespace()
-tree = dendropy.Tree.get_from_path(
-    "wQFM-best-25Nov.tre",
-    "newick",
-    taxon_namespace=taxa)
+import sys
 
 
-print("Tree: ", tree, "\n\n")
+#####################################################################################################################
 
 
-bipartitions = tree.encode_bipartitions()
 
-# print(taxa)
+""" Get sorted quartets a,b|c,d: w
+"""
+def getSortedQuartet(line):
+    stringsReplace = ["\n", ";", "(", ")"]
+    for s in stringsReplace:
+        line = line.replace(s, "") ## remove these chars
+        
+    line = line.replace(" ", ",")  ## replace WHITESPACE with COMMA    
+    arr = line.split(",") ## split by COMMA
 
-for bip in bipartitions:
-    # print(bip)
+    # arr[0:1].sort() ## Maybe not necessary
+    # arr[2:3].sort() ## Maybe not necessary
 
-    if not bip.is_trivial():
-        print(bip.leafset_as_newick_string(taxon_namespace=taxa))
-        # print(bip.split_as_newick_string(taxon_namespace=taxa, preserve_spaces=True, quote_underscores=False))
-    # print(bip.leafset_taxa(taxon_namespace=taxa))
+    return (arr[0], arr[1], arr[2], arr[3], float(arr[4]))
 
 
-# edges = tree.edges()
+""" Process quartets
+    1. Form a list of quartets
+    2. Form a list of taxa
+    3. Form a Map of <taxa: [list of qrt indices]>
+"""
+def processQuartets(inputQrtFile):
+    list_quartets = []
+    map_taxa_quartet = {}
 
-# for edge in edges:
-#     print(edge.head_node, edge.length, edge.label, edge.is_internal(), edge.is_leaf())
+    with open(inputQrtFile, 'r') as fin:
+        line = fin.readline()
+        while line:
+            quartet = getSortedQuartet(line) # (t0, t1, t2, t3, w) = getSortedQuartet(line)
+            # print(quartet)
+            list_quartets.append(quartet)
+            (t0, t1, t2, t3, w) = quartet
+            for i in range(0, 4):
+                if quartet[i] not in map_taxa_quartet:
+                    map_taxa_quartet[quartet[i]] = [] ## initialize a list for the taxon NOT in map.
+                idx_quartet = len(list_quartets) - 1
+                ## Append this INDEX for each taxon.
+                map_taxa_quartet[quartet[i]].append(idx_quartet)
+
+
+            line = fin.readline()
+
+    return list_quartets, map_taxa_quartet
+
+
+""" Main function
+"""
+def run(inputQrtFile, inputStreeFile, outputFile):
+
+    list_quartets, map_taxa_quartet = processQuartets(inputQrtFile)
+
+    for key in map_taxa_quartet:
+        print(key, map_taxa_quartet[key], "\n")
+
+
+#####################################################################################################################
+
+def printUsageExit():
+    print(f"Usage: python3 {sys.argv[0]} <input-qrt-file> <input-STree-file> <output-file>")
+    exit()
+
+
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 4:
+        printUsageExit()
+    
+    inputQrtFile = sys.argv[1]
+    inputStreeFile = sys.argv[2]
+    outputFile = sys.argv[3]
+
+    run(inputQrtFile, inputStreeFile, outputFile)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
