8b650cf421f3443cd604c09bdd36d506f3fbbffa
mahim1997 <1505022.mm@ugrad.cse.buet.ac.bd>
2021-02-06 06:19:24 +0600

    Removed map of quartets taxa as it is not used

--- WQFM/scripts-quartet-support/get_branch_qrt_support.py
+++ WQFM/scripts-quartet-support/get_branch_qrt_support.py
@@ -29,31 +29,18 @@ def getSortedQuartet(line):
 
 """ Process quartets
     1. Form a list of quartets
-    2. Form a list of taxa
-    3. Form a Map of <taxa: [list of qrt indices]>
 """
 def processQuartets(inputQrtFile):
     list_quartets = []
-    map_taxa_quartet = {}
 
     with open(inputQrtFile, 'r') as fin:
         line = fin.readline()
         while line:
             quartet = getSortedQuartet(line) # (t0, t1, t2, t3, w) = getSortedQuartet(line)
-            # print(quartet)
             list_quartets.append(quartet)
-            (t0, t1, t2, t3, w) = quartet
-            for i in range(0, 4):
-                if quartet[i] not in map_taxa_quartet:
-                    map_taxa_quartet[quartet[i]] = [] ## initialize a list for the taxon NOT in map.
-                idx_quartet = len(list_quartets) - 1
-                ## Append this INDEX for each taxon.
-                map_taxa_quartet[quartet[i]].append(idx_quartet)
-
-
             line = fin.readline()
 
-    return list_quartets, map_taxa_quartet
+    return list_quartets
 
 """ Read tree from the Species Tree file
 """
@@ -86,7 +73,7 @@ def checkQuartetSatisfied(map_bipartition, quartet):
 
 """ Function to compute Quartet Support for EACH bipartition
 """
-def get_support_value(bipartition, taxon_namespace, list_quartets, map_taxa_quartet):
+def get_support_value(bipartition, taxon_namespace, list_quartets):
     bipartitionSTR = str(bipartition)
     map_bipartition = getPartitionMap(bipartitionSTR, taxon_namespace)
     # checked_quartets_indices = [False for i in range(0, len(list_quartets))] ## initialize as False since no quartet is checked
@@ -108,7 +95,7 @@ def get_support_value(bipartition, taxon_namespace, list_quartets, map_taxa_quar
 
 """ Function to compute Quartet Support and return new tree with quartet support.
 """
-def compute_tree_QSupport(tree, list_quartets, map_taxa_quartet):
+def compute_tree_QSupport(tree, list_quartets):
     tree.encode_bipartitions() ## encode bipartitions to bitmaps
     support_values = {} ## Map for support values
 
@@ -117,7 +104,7 @@ def compute_tree_QSupport(tree, list_quartets, map_taxa_quartet):
             # print(f"\nBip = {nd.bipartition.leafset_as_newick_string(taxon_namespace=tree.taxon_namespace)}, is_trivial = {nd.bipartition.is_trivial()}")
 
             # support_values[nd.bipartition] = get_support_value(nd.bipartition) if nd.label is not None else 1.0
-            support_values[nd.bipartition] = get_support_value(nd.bipartition, tree.taxon_namespace, list_quartets, map_taxa_quartet) if nd.label is None else -1.0
+            support_values[nd.bipartition] = get_support_value(nd.bipartition, tree.taxon_namespace, list_quartets) if nd.label is None else -1.0
 
     tree.encode_bipartitions()
 
@@ -142,14 +129,14 @@ def write_output(output_tree, outputFile):
 """
 def run(inputQrtFile, inputStreeFile, outputFile):
 
-    list_quartets, map_taxa_quartet = processQuartets(inputQrtFile)
+    list_quartets = processQuartets(inputQrtFile)
     
     tree = read_tree(inputStreeFile)
     treeStr = tree.as_string("newick").strip()
     
     print(treeStr, "\n")
 
-    output_tree = compute_tree_QSupport(tree, list_quartets, map_taxa_quartet)
+    output_tree = compute_tree_QSupport(tree, list_quartets)
     output_tree = output_tree.as_string("newick").strip()
 
     output_tree = output_tree.replace("[&R] ", "") ## remove this sign
